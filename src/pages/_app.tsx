e57e09dcbb1a0e92891ca4a7fb722dd3cca99390
Obi Fortune <gabrielobi.of@gmail.com>
2023-03-25 16:27:59 +0100

    setup router transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import { RouterTransition } from '@/components';
+import { useRouterTransition } from '@/hooks';
 import { MantineProvider } from '@mantine/core';
+import { NavigationProgress } from '@mantine/nprogress';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useRouterTransition();
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <RouterTransition />
+      <NavigationProgress autoReset={true} />
       <Component {...pageProps} />
     </MantineProvider>
   );
